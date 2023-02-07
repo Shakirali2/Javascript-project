3c7fbf72921f7cfaeb87e3290f76ecc85501c36e
Shakirali2 <115064571+Shakirali2@users.noreply.github.com>
2023-02-07 07:41:37 +0500

    Add files via upload

---
+++ chapterProject.js
@@ -0,0 +1,45 @@
+// const multiplyTable = [] ; 
+// const numValue = 5 ;
+// for (let i = 0; i < numValue; i++) {
+//     const temporary = [];
+//     for (let j = 0; j < numValue; j++) {
+//         temporary.push(i*j);
+//     }
+//         multiplyTable.push(temporary);
+//     }
+    
+// console.table(multiplyTable)
+
+
+// const multiplyTable1 = [];
+// const numValue = 5;
+// for (let i = 0; i < numValue; i++) {
+//    const temporary =[];
+//    for (let j = 0; j < numValue; j++) {
+//    temporary.push(i*j);
+//    }
+//     multiplyTable1.push(temporary);
+// }
+// console.table(multiplyTable1)
+
+// const aa = [];
+// const bb = 7
+// for (let i = 0; i < bb; i++) {
+//     const temporary = [];
+//     for (let j = 0; j < bb; j++) {
+//         temporary.push(i*j);
+//     }
+//     aa.push(temporary)
+// }
+// console.table(aa)
+
+const multiplyTable11 = [];
+const numValue11 = 11;
+for (let i = 0; i < numValue11; i++) {
+    const temporary = [];
+    for (let j = 0; j < numValue11; j++) {
+        temporary.push(i*j);   
+    }
+    multiplyTable11.push(temporary);
+}
+console.table(multiplyTable11)
